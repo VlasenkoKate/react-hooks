64d489320c243672ea822fa83e92a4edbccbd200
VlasenkoKate <85071551+VlasenkoKate@users.noreply.github.com>
2021-08-05 23:13:35 +0300

    hw 18 done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Contacts from "./components/Contacts/contracts.js"
+
+function App() {
+  return (
+    <div >
+<Contacts />
+    </div>
+  );
+}
+
+export default App;
